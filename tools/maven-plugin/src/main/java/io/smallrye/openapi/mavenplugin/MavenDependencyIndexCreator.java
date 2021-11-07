775d77fafaa7345aa84f618a06c516b2f5360800
Martin Panzer <postremus1996@googlemail.com>
2021-11-07 09:46:31 +0100

    Remove index blacklist. Not worth it in my testing

--- tools/maven-plugin/src/main/java/io/smallrye/openapi/mavenplugin/MavenDependencyIndexCreator.java
+++ tools/maven-plugin/src/main/java/io/smallrye/openapi/mavenplugin/MavenDependencyIndexCreator.java
@@ -4,14 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -35,50 +29,13 @@ import com.google.common.cache.CacheBuilder;
 @Component(role = MavenDependencyIndexCreator.class, instantiationStrategy = "singleton")
 public class MavenDependencyIndexCreator {
 
-    private static final Set<String> IGNORED_GROUPIDS = new HashSet<>();
-    private static final Set<String> IGNORED_GROUPID_ARTIFACTID = new HashSet<>();
-
     private final Cache<String, IndexView> indexCache = CacheBuilder.newBuilder().build();
-
-    static {
-        IGNORED_GROUPID_ARTIFACTID.add("org.graalvm.sdk:graal-sdk");
-        IGNORED_GROUPID_ARTIFACTID.add("io.quarkus:quarkus-core");
-        IGNORED_GROUPID_ARTIFACTID.add("org.yaml:snakeyaml");
-        IGNORED_GROUPID_ARTIFACTID.add("org.wildfly.common:wildfly-common");
-        IGNORED_GROUPID_ARTIFACTID.add("org.apache.httpcomponents:httpcore");
-        IGNORED_GROUPID_ARTIFACTID.add("com.fasterxml.jackson.core:jackson-core");
-        IGNORED_GROUPID_ARTIFACTID.add("org.apache.httpcomponents:httpcore-nio");
-        IGNORED_GROUPID_ARTIFACTID.add("io.quarkus:quarkus-vertx-http");
-        IGNORED_GROUPID_ARTIFACTID.add("io.smallrye:smallrye-open-api-core");
-        IGNORED_GROUPID_ARTIFACTID.add("io.smallrye.reactive:smallrye-mutiny-vertx-core");
-        IGNORED_GROUPID_ARTIFACTID.add("commons-io:commons-io");
-        IGNORED_GROUPID_ARTIFACTID.add("org.apache.httpcomponents:httpclient");
-        IGNORED_GROUPID_ARTIFACTID.add("io.smallrye.reactive:mutiny");
-        IGNORED_GROUPID_ARTIFACTID.add("org.jboss.narayana.jta:narayana-jta");
-        IGNORED_GROUPID_ARTIFACTID.add("org.glassfish.jaxb:jaxb-runtime");
-        IGNORED_GROUPID_ARTIFACTID.add("com.github.ben-manes.caffeine:caffeine");
-        IGNORED_GROUPID_ARTIFACTID.add("com.fasterxml.jackson.core:jackson-databind");
-        IGNORED_GROUPID_ARTIFACTID.add("org.hibernate.validator:hibernate-validator");
-        IGNORED_GROUPID_ARTIFACTID.add("io.smallrye.config:smallrye-config-core");
-        IGNORED_GROUPID_ARTIFACTID.add("com.thoughtworks.xstream:xstream");
-        IGNORED_GROUPID_ARTIFACTID.add("com.github.javaparser:javaparser-core");
-
-        IGNORED_GROUPIDS.add("antlr");
-        IGNORED_GROUPIDS.add("io.netty");
-        IGNORED_GROUPIDS.add("org.drools");
-        IGNORED_GROUPIDS.add("net.bytebuddy");
-        IGNORED_GROUPIDS.add("io.vertx");
-        IGNORED_GROUPIDS.add("org.hibernate");
-        IGNORED_GROUPIDS.add("org.kie");
-        IGNORED_GROUPIDS.add("org.postgresql");
-    }
 
     @Requirement
     private Logger logger;
 
     public IndexView createIndex(MavenProject mavenProject, Boolean scanDependenciesDisable,
-            List<String> includeDependenciesScopes, List<String> includeDependenciesTypes)
-            throws MojoExecutionException, ExecutionException {
+            List<String> includeDependenciesScopes, List<String> includeDependenciesTypes) throws ExecutionException {
 
         IndexView moduleIndex = indexCache.get(buildGAVCTString(mavenProject.getArtifact()), () -> {
             try {
@@ -94,21 +51,15 @@ public class MavenDependencyIndexCreator {
 
         List<IndexView> indexes = new ArrayList<>();
         indexes.add(moduleIndex);
-        List<Map.Entry<Artifact, Duration>> durations = new ArrayList<>();
         for (Artifact artifact : mavenProject.getArtifacts()) {
             if (includeDependenciesScopes.contains(artifact.getScope())
-                    && includeDependenciesTypes.contains(artifact.getType())
-                    && !IGNORED_GROUPIDS.contains(artifact.getGroupId())
-                    && !IGNORED_GROUPID_ARTIFACTID.contains(artifact.getGroupId() + ":" + artifact.getArtifactId())) {
+                    && includeDependenciesTypes.contains(artifact.getType())) {
 
                 IndexView artifactIndex = indexCache.get(buildGAVCTString(mavenProject.getArtifact()), () -> {
-                    LocalDateTime start = LocalDateTime.now();
                     try {
                         Result result = JarIndexer.createJarIndex(artifact.getFile(), new Indexer(),
                                 false, false, false);
                         indexes.add(result.getIndex());
-                        LocalDateTime end = LocalDateTime.now();
-                        durations.add(new AbstractMap.SimpleEntry<>(artifact, Duration.between(start, end)));
 
                         return result.getIndex();
                     } catch (Exception e) {
@@ -121,14 +72,6 @@ public class MavenDependencyIndexCreator {
                     indexes.add(artifactIndex);
                 }
             }
-        }
-
-        if (logger.isDebugEnabled()) {
-            durations.sort(Map.Entry.comparingByValue());
-
-            durations.forEach(e -> {
-                logger.debug(buildGAVCTString(e.getKey()) + " " + e.getValue());
-            });
         }
 
         return CompositeIndex.create(indexes);
